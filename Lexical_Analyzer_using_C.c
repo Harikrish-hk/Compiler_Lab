0c1f75d2639908de3251bbde7243af0be3972dbd
Harikrish-hk <hk5172844@gmail.com>
2025-10-25 20:10:57 +0530

    Lexical_Analyzer_using_C.c

--- Lexical_Analyzer_using_C.c
+++ Lexical_Analyzer_using_C.c
@@ -1,45 +1,48 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
+#include<stdlib.h>
 #include<ctype.h>
-
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-    if(argc!=3)
+    if(argc!=2)
     {
-        printf("usage: %s <input file> <output file>\n",argv[0]);
+        printf("Usage: %s <input_file>\n",argv[0]);
         exit(0);
     }
     FILE *fp1,*fp2;
     fp1=fopen(argv[1],"r");
-    fp2=fopen(argv[2],"w");
-    int sl=1,line=1,flag=0,i;
+    int sl=1,line=1,i=0,flag=0;
     char lex[50],ch,ch1;
-    char key[5][20]={"main","printf","scanf","if","else"};
-    fprintf(fp2,"sl.no\tLine.no\tLexeme\tToken\n");
+    char key[15][50]={"void","main","printf","include","scanf","if","else","int"};
+    printf("sl.no\tline.no\tlexeme\ttoken\n");
     while((ch=fgetc(fp1))!=EOF)
     {
         if(ch==' '||ch=='\t');
         else if(ch=='\n')
          line++;
-        else if(ch=='{'||ch=='['||ch=='(')
+        else if(ch=='{'||ch=='('||ch=='[')
         {
-            fprintf(fp2,"%d\t%d\t%c\tOp_Brak\n",sl,line,ch);
+            printf("%d\t%d\t%c\tOpen_brack\n",sl,line,ch);
             sl++;
         }
-        else if(ch=='}'||ch==']'||ch==')')
+        else if(ch=='}'||ch==')'||ch==']')
         {
-            fprintf(fp2,"%d\t%d\t%c\tCl_Brak\n",sl,line,ch);
+            printf("%d\t%d\t%c\tClose_brack\n",sl,line,ch);
             sl++;
         }
         else if(ch=='+'||ch=='-'||ch=='*'||ch=='/')
         {
-            fprintf(fp2,"%d\t%d\t%c\tAriop\n",sl,line,ch);
+            printf("%d\t%d\t%c\tAssign_op\n",sl,line,ch);
             sl++;
         }
         else if(ch==';')
         {
-            fprintf(fp2,"%d\t%d\t%c\tSemicolon\n",sl,line,ch);
+            printf("%d\t%d\t%c\tSemicolon\n",sl,line,ch);
+            sl++;
+        }
+        else if(ch==',')
+        {
+            printf("%d\t%d\t%c\tSpl_char\n",sl,line,ch);
             sl++;
         }
         else if(ch=='&')
@@ -47,14 +50,14 @@ int main(int argc,char *argv[])
             ch1=fgetc(fp1);
             if(ch1=='&')
             {
-                fprintf(fp2,"%d\t%d\t%c%c\tRelop\n",sl,line,ch,ch1);
+                printf("%d\t%d\t%c%c\tLogical_op\n",sl,line,ch,ch1);
                 sl++;
             }
             else
             {
-                fprintf(fp2,"%d\t%d\t%c\tSpeop\n",sl,line,ch);
+                printf("%d\t%d\t%c\tSpl_char\n",sl,line,ch);
                 sl++;
-                ungetc(ch,fp1);
+                ungetc(ch1,fp1);
             }
         }
         else if(ch=='=')
@@ -62,57 +65,54 @@ int main(int argc,char *argv[])
             ch1=fgetc(fp1);
             if(ch1=='=')
             {
-                fprintf(fp2,"%d\t%d\t%c%c\tRelop\n",sl,line,ch,ch1);
+                printf("%d\t%d\t%c%c\tRel_op\n",sl,line,ch,ch1);
                 sl++;
             }
             else
             {
-                fprintf(fp2,"%d\t%d\t%c\tAssop\n",sl,line,ch);
+                printf("%d\t%d\t%c\tAss_op\n",sl,line,ch);
                 sl++;
-                ungetc(ch,fp1);
+                ungetc(ch1,fp1);
             }
         }
         else if(isdigit(ch))
         {
             i=0;
             lex[i++]=ch;
-            do
-            {
-                ch1=fgetc(fp1);
-                lex[i++]=ch1;
-                if(ch1=='.')
+            do{
+                ch=fgetc(fp1);
+                lex[i++]=ch;
+                if(ch=='.')
                  flag=1;
-            }while(ch1=='.'||isdigit(ch1));
+            }while(isdigit(ch)||ch=='.');
             lex[--i]='\0';
-            ungetc(ch1,fp1);
-            if(flag==1)
+            ungetc(ch,fp1);
+            if(flag==0)
             {
-                fprintf(fp2,"%d\t%d\t%s\tFloat\n",sl,line,lex);
+                printf("%d\t%d\t%s\tDigit\n",sl,line,lex);
                 sl++;
             }
-            else
+            else if(flag==1)
             {
-                fprintf(fp2,"%d\t%d\t%s\tDigit\n",sl,line,lex);
+                printf("%d\t%d\t%s\tFloat_point\n",sl,line,lex);
                 sl++;
             }
             flag=0;
-            i=0;
         }
-        
         else if(isalpha(ch))
         {
             i=0;
             lex[i++]=ch;
-            do
-            {
-                ch1=fgetc(fp1);
-                lex[i++]=ch1;
-            }while(isdigit(ch1)||isalpha(ch1));
+            do{
+                ch=fgetc(fp1);
+                lex[i++]=ch;
+            }while(isalpha(ch)||isdigit(ch));
             lex[--i]='\0';
-            ungetc(ch1,fp1);
-            for(int j=0;j<5;j++)
+            ungetc(ch,fp1);
+
+            for(i=0;i<8;i++)
             {
-                if(strcmp(key[j],lex)==0)
+                if(strcmp(key[i],lex)==0)
                 {
                     flag=1;
                     break;
@@ -120,21 +120,36 @@ int main(int argc,char *argv[])
             }
             if(flag==1)
             {
-                fprintf(fp2,"%d\t%d\t%s\tKeywrd\n",sl,line,lex);
+                printf("%d\t%d\t%s\tKeyword\n",sl,line,lex);
                 sl++;
             }
-            else
-                {
-                    fprintf(fp2,"%d\t%d\t%s\tIdentifier\n",sl,line,lex);
-                    sl++;
-                }
+            else if(flag==0)
+            {
+                printf("%d\t%d\t%s\tIdentifier\n",sl,line,lex);
+                sl++;
+            }
+            flag=0;
         }
         else
         {
-            fprintf(fp2,"%d\t%d\t%c\tUnrecg\n",sl,line,ch);
-                    sl++;
+            printf("%d\t%d\t%c\tUnrecog_char\n",sl,line,ch);
+            sl++;
         }
     }
-    fclose(fp1);
-    fclose(fp2);
 }
+
+
+/*
+
+--------------------input.txt-------------------
+
+#include<stdio.h>
+int main()
+{
+    int a=2.31;
+    int b=a+10;
+    if(a==b)
+     printf("Error");
+} 
+     
+*/
